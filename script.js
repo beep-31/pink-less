36d11ffe7e0421bccb44871d635de52ea3f28347
Alex <ascheglov17@gmail.com>
2020-04-19 00:03:20 +0200

    Slider's bug was fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function(){
+// window.addEventListener("DOMContentLoaded", function(){
     let burger = document.querySelector('.burger-item'),
     nav = document.querySelector('#nav');
     burger.addEventListener('click', function(){
@@ -8,10 +8,13 @@ window.addEventListener("DOMContentLoaded", function(){
             nav.style.opacity = 0;
         }
     });
-
     let review_dots = document.querySelectorAll('#review-dot'),
-        review_items = document.querySelectorAll('.review__item.slider-item'),
+        review_items = document.querySelectorAll('.slider-item'),
         dot_parent = document.querySelector('.reviews__dots');
+
+    hideContent(review_items.length);
+    showContent(0);
+    review_items[0].style.opacity = 1;
 
     dot_parent.addEventListener('click', function(event){
         let target = event.target;
@@ -20,6 +23,7 @@ window.addEventListener("DOMContentLoaded", function(){
                 if(target==review_dots[i]){
                     hideContent(review_items.length);
                     showContent(i);
+                    fadeSlider(i);
                 }
             }
         }
@@ -27,18 +31,32 @@ window.addEventListener("DOMContentLoaded", function(){
 
     function hideContent(n){
         for (let i = 0; i < review_items.length; i++){
-            review_items[i].classList.remove('slider-item--active');
+            review_items[i].style.opacity=0;
+            review_items[i].style.display='none';
             review_dots[i].classList.remove('dot--active');
         }
     }
 
     function showContent(n){
-        if(!review_items[n].classList.contains('slider-item--active')){
-            review_items[n].classList.add('slider-item--active');
-            review_dots[n].classList.add('dot--active');
-        }
+        review_items[n].style.display = 'flex';
+        fadeSlider(n);
+        review_dots[n].classList.add('dot--active');
+
+    }
+
+    var opacity = 0.1;
+    function fadeSlider(i){
+        setTimeout(function(){
+            review_items[i].style.opacity+=opacity;
+            opacity+=0.1;
+            if(opacity<1){
+                fadeSlider(i);
+            } else{
+                clearTimeout();
+            }
+        }, 20);
+
     }
     
-    
-});
+// });
 
