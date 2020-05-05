f4ed17f9d3b5567869e85f3ecfe3362e64a58ea1
Alex <ascheglov17@gmail.com>
2020-05-05 18:14:00 +0200

    The closing mark was added for the header on mobile and tablet devices

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,17 @@
 window.addEventListener('DOMContentLoaded', function(){
     let burger = document.querySelector('.burger-item'),
-    nav = document.querySelector('#nav');
-    console.log(nav);
-    console.log(burger);
+        nav = document.querySelector('#nav'),
+        opened = 'burger-item--opened',
+        closed = 'burger-item--closed';
     burger.addEventListener('click', function(){
     if(nav.style.opacity==0){
         nav.style.opacity = 1;
+        burger.classList.remove(closed);
+        burger.classList.add(opened);
     } else{
         nav.style.opacity = 0;
+        burger.classList.remove(opened);
+        burger.classList.add(closed);
     }
 });
 });
